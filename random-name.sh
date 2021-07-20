d54a0fc152989a15f1a4d4d2a8c55580a107eb49
Ivan Yastrebov <easy.quest@mail.ru>
2021-07-20 18:17:44 +0300

    изменено:      .github/workflows/github-actions-demo.yml 	изменено:      random-name.sh

--- random-name.sh
+++ random-name.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-prefix=temp
+prefix=txt
 suffix=`eval date +%s`  # Ключ "+%s" характерен для GNU-версии 'date'.
-filename=$prefix.$suffix
-echo $filename
+filename=$suffix.$prefix
+# filename=$prefix.$suffix
+# echo $filename
 #  Прекрасный способ получения "уникального" имени для временного файла,
 #+ даже лучше, чем с использованием $$.
-
-
+touch $filename
+exa -a --color=always --group-directories-first --icons
